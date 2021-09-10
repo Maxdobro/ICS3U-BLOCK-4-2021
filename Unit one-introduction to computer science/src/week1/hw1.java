d97d9fcbe0817166f6fb16efd466752a065428a7
Max Dobrowolski <Mdobrowolski@bayviewglen.ca>
2021-09-10 14:54:02 -0400

    icons

---
+++ Unit one-introduction to computer science/src/week1/hw1.java
@@ -0,0 +1,15 @@
+package week1;
+
+public class hw1 {
+    public static void main(String[] args) {
+        System.out.print("AP ");
+        System.out.print("CS ");
+        System.out.print("A ");
+        System.out.println("Rocks! ");
+        System.out.println("Hello World ");
+    }
+    
+    
+
+    
+}
---
+++ Unit one-introduction to computer science/src/week1/hw2.java
@@ -0,0 +1,10 @@
+package week1;
+
+public class hw2 {
+    public static void main(String[] args) {
+        System.out.println("This is a string litteral");
+        System.out.println("ABCD123#$%abc");
+        System.out.println("Have a GREAT day!!");
+        System.out.println("AP CS A Rocks!!!");
+    }
+}
